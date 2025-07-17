06ed3d1feaf4c1cf35df1a284616a1af3606a6ee
Tia <tiamdarvell@gmail.com>
2025-07-17 17:00:58 +1000

    Template-fakebook

--- osint-project/fakebook/assets/js/main.js
+++ osint-project/fakebook/assets/js/main.js
@@ -1,16 +1,47 @@
-function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+function openMainTab(evt, tabName) {
+  const tabcontents = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontents.length; i++) {
+    tabcontents[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+
+  const tablinks = document.getElementsByClassName("navlinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+
+    // Reset image to default
+    const img = tablinks[i].querySelector("img");
+    if (img && img.dataset.icon) {
+      img.src = img.dataset.icon;
+    }
   }
-  document.getElementById(tabName).style.display = "flex";
-  evt.currentTarget.className += " active";
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.classList.add("active");
+
+  // Change image to active version
+  const activeImg = evt.currentTarget.querySelector("img");
+  if (activeImg && activeImg.dataset.iconActive) {
+    activeImg.src = activeImg.dataset.iconActive;
+  }
 }
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+// Function to handle profile navigation tab switching
+function openProfileTab(evt, tabName) {
+  const tabcontents = document.getElementsByClassName("profile-tabcontent");
+  for (let i = 0; i < tabcontents.length; i++) {
+    tabcontents[i].style.display = "none";
+  }
+
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  document.getElementById(tabName).style.display = "flex";
+  evt.currentTarget.classList.add("active");
+}
+
+// Optional: Set default open profile tab
+window.onload = function () {
+  document.getElementById("defaultOpen")?.click();
+};
